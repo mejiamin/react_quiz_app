031500385a67efe5f5aed6f6205195d2f34664e3
Meji Amin <mokhmadmishiev@gmail.com>
2026-06-25 11:15:30 +0300

    update StartPage.jsx

--- src/pages/start/StartPage.jsx
+++ src/pages/start/StartPage.jsx
@@ -1,47 +1,48 @@
 import { useLocation, useNavigate } from 'react-router-dom'
 import styles from './startPage.module.css'
 
+
+
 export const StartPage = () => {
   const location = useLocation()
   const navigate = useNavigate()
 
   const totalCount = location.state?.totalCount || 10
-  const currentQuestionIndex = 0
 
-  const handleFakeEndGame = () => {
-    const fakeHistory = [
-      {
-        question: "Флаг какой страны изображен?",
-        correctAns: "Нигерия",
-        userAns: "Нигерия",
-        isCorrect: true,
-        flag: "https://upload.wikimedia.org/wikipedia/commons/7/79/Flag_of_Nigeria.svg"
-      },
-      {
-        question: "Флаг какой страны изображен?",
-        correctAns: "Кения",
-        userAns: "Мали",
-        isCorrect: false,
-        flag: "https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Kenya.svg"
-      }
-    ]
 
-    navigate('/results', { state: { history: fakeHistory } })
-  }
 
   return (
     <div className={styles.card}>
       <div className={styles.progress}>
-        Вопрос {currentQuestionIndex + 1} из {totalCount}
+        Вопрос {1} из ...length
       </div>
 
-      <div className={styles.gameBox}>
-        <p>[Здесь в Уроке 3 появится флаг и варианты ответов]</p>
+      <h2 className={styles.questionText}>Страна</h2>
 
-        <button onClick={handleFakeEndGame} className={styles.fakeBtn}>
-          Завершить игру (тест результатов)
+      <img
+        alt="Флаг страны"
+        className={styles.flagImage}
+      />
+
+      <div className={styles.optionsGrid}>
+
+        {/* map */}
+        <button
+          className={styles.btnClass}
+        >
+          option
         </button>
+
       </div>
+
+      {true && (
+        <button className={styles.nextBtn}>
+          {false
+            ? 'Посмотреть результаты'
+            : 'Дальше →'
+          }
+        </button>
+      )}
     </div>
   )
 }
